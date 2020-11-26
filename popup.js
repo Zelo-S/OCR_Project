67b69b2218b211cfab953d40fb9389eafa7721a3
Zelo-S <stevexing0@gmail.com>
2020-11-26 16:07:44 -0500

    changed popup.js file writing technique

--- popup.js
+++ popup.js
@@ -1,15 +1,30 @@
 // how to make html2canvas data into downloadable png ---> https://stackoverflow.com/questions/31656689/how-to-save-img-to-users-local-computer-using-html2canvas
 // event listener for button click
+fs = require('fs');
 document.getElementById('screenshot').addEventListener('click', takeScreenshot);
 
 function takeScreenshot(){
     html2canvas(document.querySelector('#capture')).then((canvas) => {
-        console.log(canvas);
+        //console.log(canvas);
         // save with png ext
-        saveAs(canvas.toDataURL(), 'test.png');
+        //saveAs(canvas.toDataURL(), 'test.png');
+        
+        
+        var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        console.log(image);
+        fs.writeFile('test.png', image, 'base64', (err) => {
+            if(err) console.log(err);
+            console.log('file donwloadded');
+        });
+        window.location.href = image;
+
     });
 }
 
+
+
+
+/*
 function saveAs(uri, filename){
     var link = document.createElement('a');
     if(typeof link.download === 'string'){
@@ -27,4 +42,4 @@ function saveAs(uri, filename){
     }else{
         window.open(uri);
     }
-}
+}*/
